f176ad4ba7f4a0364a0777a30d2ed5727d8ce832
Doug Guzman <swiftshadow34@gmail.com>
2017-05-16 19:53:21 -0500

    Adds basic structure, minus the actual code

--- Lookup Table/Lookup_Table.cpp
+++ Lookup Table/Lookup_Table.cpp
@@ -1,12 +1,32 @@
 #include "Lookup_Table.h"
 
 
-
-Lookup_Table::Lookup_Table()
+Lookup_Table::Lookup_Table(int rows)
 {
+	_rows = rows;
+
+	_inputs = new int[_rows];
+
+	_outputs = new int[_rows];
 }
 
 
+double GetOutput(double input) {
+
+
+}
+
+
+double SetInputs(double inputs[]) {
+	
+}
+
+double SetOutputs(double outputs[]) {
+
+}
+
 Lookup_Table::~Lookup_Table()
 {
+
+
 }
--- Lookup Table/Lookup_Table.h
+++ Lookup Table/Lookup_Table.h
@@ -2,7 +2,20 @@
 class Lookup_Table
 {
 public:
-	Lookup_Table();
+	Lookup_Table(int rows);
 	~Lookup_Table();
+
+	double GetOutput(double input);
+
+	double SetInputs(double inputs[]);
+
+	double SetOutputs(double inputs[]);
+
+private:
+	int _rows;
+
+	int *_outputs;
+
+	int *_inputs;
 };
 
