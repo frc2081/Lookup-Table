70950e28b06bc18ed3570060bc8086bfd7a37d5a
Lunar Dust <infinitycreeper1@gmail.com>
2017-05-24 09:43:32 -0500

    Allows for reading input values from file

--- Lookup Table/Lookup_Table_Input_Reader.h
+++ Lookup Table/Lookup_Table_Input_Reader.h
@@ -1,8 +1,22 @@
 #pragma once
+#include <iostream>
+#include <fstream>
+#include "Lookup_Table.h"
 class Lookup_Table_Input_Reader
 {
 public:
-	Lookup_Table_Input_Reader();
+	// Array of Points that will be filled upon class' instantiation
+	Lookup_Table::Point *Points;
+	// Will always need to start at row 1
+	int Rows = 1;
+
+	// Constructor and deconstructor
+	Lookup_Table_Input_Reader(char*);
 	~Lookup_Table_Input_Reader();
+
+private:
+	void CountRows();
+	void MakePoints();
+	char *_file;
 };
 
